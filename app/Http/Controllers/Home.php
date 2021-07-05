1bd036cf2e3f66f5b105a0489cd0e807bbfcf14a
eMiracle <miraclechibuzo@gmail.com>
2021-07-05 13:55:59 +0100

    init

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -15,7 +15,7 @@ class Home extends Controller
         $tempId = bin2hex(random_bytes(40));
 
         $connection = new TwitterOAuth("Fj1skBBtAUuvuuYHJE0c3vDcK", "uHaXF3uF7e4tQ3FkVOfYf7uetPZp8xUERWguZ5WRqnSET7i1BB");
-        $requestToken = $connection->oauth('oauth/request_token', array('oauth_callback' => '127.0.0.1:8000?user='.$tempId));
+        $requestToken = $connection->oauth('oauth/request_token', array('oauth_callback' => 'https://twitter-stateless.herokuapp.com/callback?user='.$tempId));
 
         Cache::put($tempId, $requestToken['oauth_token_secret'], 1);
         $url = $connection->url('oauth/authorize', array('oauth_token' => $requestToken['oauth_token']));
