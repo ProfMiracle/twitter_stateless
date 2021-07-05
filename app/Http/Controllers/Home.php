6dcec66d12ca0451046568c3a8c2f45dc4b798ee
eMiracle <miraclechibuzo@gmail.com>
2021-07-05 14:26:50 +0100

    init

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -20,13 +20,16 @@ class Home extends Controller
 
         Cache::put($tempId, $requestToken['oauth_token_secret'], 1);
         $url = $connection->url('oauth/authorize', array('oauth_token' => $requestToken['oauth_token']));
-        print function_exists('curl_exec') ? 'curl_exec is enabled' : 'curl_exec is disabled';
+
         return $url;
     }
 
     public function callb(Request $request)
     {
         $connection = new TwitterOAuth("Fj1skBBtAUuvuuYHJE0c3vDcK", "uHaXF3uF7e4tQ3FkVOfYf7uetPZp8xUERWguZ5WRqnSET7i1BB", $request->oauth_token, Cache::get($request->user));
+        echo "<pre>";
+        var_dump(Cache::get($request->user));
+        echo "</pre>";
         $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $request->oauth_verifier]);
 
         $connection = new TwitterOAuth(config('services.twitter.client_id'), config('services.twitter.client_secret'), $access_token['oauth_token'], $access_token['oauth_token_secret']);
