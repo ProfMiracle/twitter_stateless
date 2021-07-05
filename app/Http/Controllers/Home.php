836c89ec612124ec6663cf34f6bd13d19a2b59a6
eMiracle <miraclechibuzo@gmail.com>
2021-07-05 13:02:00 +0100

    init

---
+++ app/Http/Controllers/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Cache;
+
+class Home extends Controller
+{
+    public function index()
+    {
+        $tempId = bin2hex(random_bytes(40));
+
+        $connection = new TwitterOAuth("Fj1skBBtAUuvuuYHJE0c3vDcK", "uHaXF3uF7e4tQ3FkVOfYf7uetPZp8xUERWguZ5WRqnSET7i1BB");
+        $requestToken = $connection->oauth('oauth/request_token', array('oauth_callback' => '127.0.0.1:8000?user='.$tempId));
+
+        Cache::put($tempId, $requestToken['oauth_token_secret'], 1);
+        $url = $connection->url('oauth/authorize', array('oauth_token' => $requestToken['oauth_token']));
+        return $url;
+    }
+}
